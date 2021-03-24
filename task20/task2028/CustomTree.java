6e3c8a2d6e04ece892032c35e5bb2c720b6b9bdb
Antony_Plsh <m13link@gmail.com>
2021-03-24 13:31:23 +1000

    add task2028 part4

--- task20/task2028/CustomTree.java
+++ task20/task2028/CustomTree.java
@@ -2,6 +2,7 @@ package com.javarush.task.task20.task2028;
 
 import java.io.Serializable;
 import java.util.AbstractList;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -10,6 +11,51 @@ import java.util.List;
 */
 
 public class CustomTree extends AbstractList<String> implements Cloneable, Serializable {
+    ArrayList<Entry<String>> list = new ArrayList<>();
+    int count = 0;
+    Entry<String> root;
+
+
+    public CustomTree() {
+        root = new Entry<>("root");
+        list.add(root);
+    }
+
+    @Override
+    public boolean add(String elementName) {
+        Entry<String> newEntry = new Entry<>(elementName);
+        for (Entry<String> entry : list ) {
+            if (entry.availableToAddLeftChildren) {
+                entry.leftChild = newEntry;
+                entry.availableToAddLeftChildren = false;
+                newEntry.parent = entry;
+                break;
+            }
+            else if (entry.availableToAddRightChildren) {
+                entry.rightChild = newEntry;
+                entry.availableToAddRightChildren = false;
+                newEntry.parent = entry;
+                break;
+            }
+        }
+        count++;
+        return list.add(newEntry);
+    }
+
+    public String getParent(String elementName) {
+        String nameParent = null;
+        for (Entry<String> entry : list) {
+            if (!entry.getName().equals("root")) {
+
+                if (entry.getName().equals(elementName)) {
+                    nameParent = entry.parent.getName();
+                    break;
+                }
+            }
+
+        }
+        return nameParent;
+    }
 
     @Override
     public String get(int index) {
@@ -18,7 +64,7 @@ public class CustomTree extends AbstractList<String> implements Cloneable, Seria
 
     @Override
     public int size() {
-        return 0;
+        return count;
     }
 
     public String set(int index, String element) {
@@ -47,19 +93,30 @@ public class CustomTree extends AbstractList<String> implements Cloneable, Seria
 
 
     static class Entry<T> implements Serializable {
+
         String elementName;
-        boolean availableToAddLeftChildren = true;
-        boolean availableToAddRightChildren = true;
+        boolean availableToAddLeftChildren;
+        boolean availableToAddRightChildren;
+
         Entry<T> parent;
         Entry<T> leftChild;
         Entry<T> rightChild;
 
         public Entry(String elementName) {
             this.elementName = elementName;
+            availableToAddRightChildren = true;
+            availableToAddLeftChildren = true;
+
+        }
+
+        public String getName() {
+            return this.elementName;
         }
 
         public boolean isAvailableToAddChildren() {
             return (availableToAddLeftChildren | availableToAddRightChildren);
         }
     }
+
+
 }
