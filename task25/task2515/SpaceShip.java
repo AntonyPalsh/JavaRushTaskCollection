458550548ea051879eb294f235c961e65230d103
Antony_Plsh <m13link@gmail.com>
2021-04-16 14:38:15 +1000

    add task 2515 part 13

--- task25/task2515/SpaceShip.java
+++ task25/task2515/SpaceShip.java
@@ -13,6 +13,23 @@ public class SpaceShip extends BaseObject{
     }
 
     public void moveRight() {
-        dx++;
+        dx = 1;
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+    }
+
+    @Override
+    public void move() {
+        x = x + dx;
+        checkBorders(radius, Space.game.getWidth() - radius + 1, 1, Space.game.getHeight() + 1);
+    }
+
+    public void fire() {
+        Rocket rocketLeft = new Rocket(x - 2, y);
+        Rocket rocketRight = new Rocket(x + 2, y);
+        Space.game.getRockets().add(rocketLeft);
+        Space.game.getRockets().add(rocketRight);
     }
 }
